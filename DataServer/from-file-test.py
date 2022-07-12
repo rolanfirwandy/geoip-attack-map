eedd35576eb1613aa1889b34ed129b2098069f9d
Rolan Firwandy <rolanfirwandy@gmail.com>
2022-07-12 18:22:39 +0700

    ip-api.com example implementation

--- DataServer/from-file-test.py
+++ DataServer/from-file-test.py
@@ -28,24 +28,23 @@ attack_data = '{},{},{},{},{},{}'.format(
 
 print(attack_data)
 
-response = urlopen('https://ipwho.is/' + sourceAddress)
-jsondata = json.load(response)
-for x, y in jsondata.items():
-    if (x != 'flag'):
-        print(x, y)
-print("---------")
-print("Success: " + str(jsondata['success']))
-if (jsondata['success']):
-    print("IP: " + jsondata['ip'])
-    print("Continent: " + jsondata['continent'])
-    print("Country: " + jsondata['country'])
-    print("Region: " + jsondata['region'])
-    print("City: " + jsondata['city'])
-    print("Latitude: " + str(jsondata['latitude']))
-    print("Longitude: " + str(jsondata['longitude']))
-else:
-    print("Message: " + jsondata['message'])
-
+# response = urlopen('https://ipwho.is/' + sourceAddress)
+# jsondata = json.load(response)
+# for x, y in jsondata.items():
+#     if (x != 'flag'):
+#         print(x, y)
+# print("---------")
+# print("Success: " + str(jsondata['success']))
+# if (jsondata['success']):
+#     print("IP: " + jsondata['ip'])
+#     print("Continent: " + jsondata['continent'])
+#     print("Country: " + jsondata['country'])
+#     print("Region: " + jsondata['region'])
+#     print("City: " + jsondata['city'])
+#     print("Latitude: " + str(jsondata['latitude']))
+#     print("Longitude: " + str(jsondata['longitude']))
+# else:
+#     print("Message: " + jsondata['message'])
 
 # response = requests.get('https://ipwho.is/' + sourceAddress)
 # print("Response code: " + str(response.status_code))
@@ -62,13 +61,26 @@ else:
 # print("Latitude: " + str(jsondata['latitude']))
 # print("Longitude: " + str(jsondata['longitude']))
 
-
 #response = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=cd2accb3c58c4396a6415306bf169608&fields=continent,country,city,latitude,longitude&ip_address=' + sourceAddress)
 #print(response.status_code)
 #print(response.json())
 
 #loc = requests.get('https://ipapi.co/' + sourceAddress + '/json/')
 #print (loc.json())
+
+response = urlopen('http://ip-api.com/json/' + sourceAddress)
+jsondata = json.load(response)
+print(jsondata)
+print("---------")
+print("IP: " + jsondata['query'])
+print("City: " + jsondata['city'])
+print("Continent: " + jsondata['regionName'])
+print("Continent Code: " + jsondata['region'])
+print("Country: " + jsondata['country'])
+print("Country Code: " + jsondata['countryCode'])
+print("Latitude: " + str(jsondata['lat']))
+print("Longitude: " + str(jsondata['lon']))
+print("Postal Code: " + str(jsondata['zip']))
 
 # Closing file
 f.close()
