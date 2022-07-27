f97d549ca731abab9df2e6927235a4a70425ce93
Rolan Firwandy <rolanfirwandy@gmail.com>
2022-07-27 15:31:27 +0700

    Add elastic logic for kl & palo alto

--- DataServer/ElasticService.py
+++ DataServer/ElasticService.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 def main():
+	warnings.filterwarnings("ignore")
 	kpu_index = 'coba1_kpu'
 	kl_index = 'coba_kl'
 	pa_index = 'coba_palo_alto'
@@ -17,33 +18,76 @@ def main():
 	print(es.ping())
 
 	while True:
-		time.sleep(4)
-		# Get KPU data
+		time.sleep(6)
 
+		# Get KPU data
 		resp = es.search(index=kpu_index, query={"match_all":{}}, size=4, sort={"@timestamp":{"order":"desc"}})
 		print("Got %d Hits:" % resp['hits']['total']['value'])
 		for hit in resp['hits']['hits']:
-		    sourceAddress = hit['_source']['fortinet']['firewall']['srcip']
-		    sourcePort = hit['_source']['fortinet']['firewall']['srcport']
-		    destinationAddress = hit['_source']['fortinet']['firewall']['dstip']
-		    destinationPort = hit['_source']['fortinet']['firewall']['dstport']
-		    applicationProtocol = hit['_source']['fortinet']['firewall']['service']
-		    destinationServiceName = hit['_source']['fortinet']['firewall']['attack']
+		    kpuSourceAddress = hit['_source']['fortinet']['firewall']['srcip']
+		    kpuSourcePort = hit['_source']['fortinet']['firewall']['srcport']
+		    kpuDestinationAddress = hit['_source']['fortinet']['firewall']['dstip']
+		    kpuDestinationPort = hit['_source']['fortinet']['firewall']['dstport']
+		    kpuApplicationProtocol = hit['_source']['fortinet']['firewall']['service']
+		    kpuDestinationServiceName = hit['_source']['fortinet']['firewall']['attack']
 
-		    attack_data = '{},{},{},{},{},{}'.format(
-		                                    sourceAddress,
-		                                    destinationAddress,
-		                                    sourcePort,
-		                                    destinationPort,
-		                                    applicationProtocol,
-		                                    destinationServiceName
+		    kpu_attack_data = '{},{},{},{},{},{}'.format(
+		                                    kpuSourceAddress,
+		                                    kpuDestinationAddress,
+		                                    kpuSourcePort,
+		                                    kpuDestinationPort,
+		                                    kpuApplicationProtocol,
+		                                    kpuDestinationServiceName
 		                                    )
 
-		    print(attack_data)
-		    syslog.syslog(attack_data)
+		    syslog.syslog(kpu_attack_data)
 		    time.sleep(0.2)
 
 		# Get KL data
+		resp = es.search(index=kl_index, query={"match_all":{}}, size=4, sort={"@timestamp":{"order":"desc"}})
+		print("Got %d Hits:" % resp['hits']['total']['value'])
+		for hit in resp['hits']['hits']:
+		    klSourceAddress = hit['_source']['cef']['extensions']['sourceAddress']
+		    klSourcePort = hit['_source']['cef']['extensions']['sourcePort']
+		    klDestinationAddress = hit['_source']['cef']['extensions']['destinationAddress']
+		    klDestinationPort = hit['_source']['cef']['extensions']['destinationPort']
+		    klApplicationProtocol = hit['_source']['cef']['extensions']['applicationProtocol']
+		    klDestinationServiceName = hit['_source']['cef']['extensions']['destinationServiceName']
+
+		    kl_attack_data = '{},{},{},{},{},{}'.format(
+		                                    klSourceAddress,
+		                                    klDestinationAddress,
+		                                    klSourcePort,
+		                                    klDestinationPort,
+		                                    klApplicationProtocol,
+		                                    klDestinationServiceName
+		                                    )
+
+		    syslog.syslog(kl_attack_data)
+		    time.sleep(0.2)
+
+		# Get Palo Alto data
+		resp = es.search(index=pa_index, query={"match_all":{}}, size=4, sort={"@timestamp":{"order":"desc"}})
+		print("Got %d Hits:" % resp['hits']['total']['value'])
+		for hit in resp['hits']['hits']:
+		    paSourceAddress = hit['_source']['source']['ip']
+		    paSourcePort = hit['_source']['source']['port']
+		    paDestinationAddress = hit['_source']['destination']['ip']
+		    paDestinationPort = hit['_source']['destination']['port']
+		    paApplicationProtocol = hit['_source']['network']['transport']
+		    paDestinationServiceName = hit['_source']['service']['type']
+
+		    pa_attack_data = '{},{},{},{},{},{}'.format(
+		                                    paSourceAddress,
+		                                    paDestinationAddress,
+		                                    paSourcePort,
+		                                    paDestinationPort,
+		                                    paApplicationProtocol,
+		                                    paDestinationServiceName
+		                                    )
+
+		    syslog.syslog(pa_attack_data)
+		    time.sleep(0.2)
 
 if __name__ == '__main__':
     try:
